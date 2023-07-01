fe286a52e193f662d78e1d9007761ec48707970c
lucasnperez <lucasgperez2007@gmail.com>
2023-07-01 16:52:00 -0300

    first commit

---
+++ nome.js
@@ -0,0 +1,4 @@
+const nome = "lucas";
+const sobrenome = "perez";
+
+console.log(`Meu nome é ${nome} e meu sobrenome é ${sobrenome} `)
---
+++ soma.js
@@ -0,0 +1,11 @@
+
+let nota1 = 10;
+let nota2 = 5;
+let nota3 = 8;
+let nota4 = 3;
+
+
+media = (nota1 + nota2 + nota3 + nota4)/ 4
+
+console.log(media);
+// 
